099bcd0e03ed051968ac1acc34bb5d8429c00088
자연이-깃 <wkdus1999@gmail.com>
2024-10-22 11:12:44 +0900

    Feat : 네비게이션바 추가 구현 - 자기소개 상세 페이지에서도 적용

--- team_intro_2/lib/profile_datail.dart
+++ team_intro_2/lib/profile_datail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:team_intro/main.dart';
+import 'package:team_intro/team_intro.dart';
 
 class profileDetailPage extends StatelessWidget {
   final String name;
@@ -16,6 +18,8 @@ class profileDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int _selectedIndex = 1;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -73,6 +77,44 @@ class profileDetailPage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar( // 페이지 하단의 네비게이션 바
+        type: BottomNavigationBarType.fixed, // 아이템 정렬을 균일하게 설정
+        selectedItemColor: Colors.pink, // 선택된 아이템의 색상
+        unselectedItemColor: Colors.grey, // 선택되지 않은 아이템의 색상도 핑크로 설정
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyHomePage(title : "오늘도 민첩한 하루 되세요.")), // 홈 페이지로 이동
+            );
+          }
+          else if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TeamIntroductionApp(title : "오늘도 민첩한 하루 되세요.")), // 홈 페이지로 이동
+            );
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: '팀 소개',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment), // 프로젝트를 대체할 아이콘
+            label: '프로젝트',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_mail),
+            label: '연락처',
+          ),
+        ],
       ),
     );
   }
